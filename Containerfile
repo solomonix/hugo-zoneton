5a437365222650d59d9a247c26c8da134979ce7f
Harlen Compton <harlen@web.harlencompton.com>
2024-01-20 15:07:45 -0500

    fixing deploy script and container

--- Containerfile
+++ Containerfile
@@ -5,9 +5,3 @@ LABEL org.zonetonlodge.image.authors="harlen@harlencompton.com"
 # install hugo
 RUN dnf install --assumeyes hugo
 
-# run the hugo build on /src
-WORKDIR /src
-RUN mv /src/public/.gitignore /tmp
-RUN rm -rf /src/public/*
-RUN mv /tmp/.gitignore /src/public/.gitignore
-CMD exec hugo
--- deploy.sh
+++ deploy.sh
@@ -6,4 +6,9 @@ podman build --tag hugo-zoneton:latest -f Containerfile .
 # TODO: re-deploy the service timer if necessary
 
 # Build the static site using the image
-podman run hugo-zoneton -v /opt/hugo-zoneton:z,rw
+podman run -v /opt/hugo-zoneton:/src:z,rw --rm hugo-zoneton /bin/bash -c 'cd /src && hugo -b "https://zonetonlodge.org/" --minify'
+
+# Copy the resulting public directory to /var/www
+rm -rf /var/www/hugo-zoneton/*
+cp -R /opt/hugo-zoneton/public/* /var/www/hugo-zoneton/
+
